510e183935b058168f5fcf69edcd5c05c8dfbc0e
soczystys <34406205+soczystys@users.noreply.github.com>
2023-06-09 16:21:24 +0200

    Calc

--- src/main/java/com/delta/Course/Calculator.java
+++ src/main/java/com/delta/Course/Calculator.java
@@ -1,2 +1,11 @@
-package com.delta.Course;public class Calculator {
+package com.delta.Course;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 }
--- src/main/java/com/delta/Course/FirstClass.java
+++ src/main/java/com/delta/Course/FirstClass.java
@@ -1,4 +1,11 @@
 package com.delta.Course;
 
+import static com.delta.Course.Calculator.add;
+import static com.delta.Course.Calculator.subtract;
+
 public class FirstClass {
+    public static void main(String[] args) {
+        add(2,7);
+        subtract(2,5);
+    }
 }
